1a125c6204feaa576a5c236144e3ce9dcbf328a1
Arthur Béquié <arthurbequie@protonmail.com>
2022-06-05 18:40:31 +0200

    WIP: handleSubmit form create employee

--- src/pages/NewEmployee/index.tsx
+++ src/pages/NewEmployee/index.tsx
@@ -1,41 +1,58 @@
-import React from "react"
+import React, { useRef } from "react"
+import { useDispatch } from "react-redux";
+
 
 const NewEmployee: React.FC = () => {
+    const dispatch = useDispatch();
+    const firstNameRef = useRef<HTMLInputElement>(null);
+    const lastNameRef = useRef<HTMLInputElement>(null);
+    const dateOfBirthRef = useRef<HTMLInputElement>(null);
+    const startDateRef = useRef<HTMLInputElement>(null);
+    const streetRef = useRef<HTMLInputElement>(null);
+    const cityRef = useRef<HTMLInputElement>(null);
+    const stateRef = useRef<HTMLSelectElement>(null);
+    const zipCodeRef = useRef<HTMLInputElement>(null);
+    const departmentRef = useRef<HTMLSelectElement>(null);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(e.target)
+    }
 
     return (
         <main className="new-employee">
             <section className="new-employee-form-section">
                 <h1 className="create-employee-form-header">Add new employee</h1>
-                <form className="create-employee-form" onSubmit={(e) => console.log(e)}>
+                <form className="create-employee-form" onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
                     <div className="create-employee-form-input">
                         <label htmlFor="firstName">First name</label>
-                        <input type="text" id="firstName" />
+                        <input ref={firstNameRef} type="text" id="firstName" />
                     </div>
                     <div className="create-employee-form-input">
                         <label htmlFor="lastName">Last name</label>
-                        <input type="text" id="lastName" />
+                        <input ref={lastNameRef} type="text" id="lastName" />
                     </div>
                     <div className="create-employee-form-input">
                         <label htmlFor="dateOfBirth">Date of birth</label>
-                        <input type="date" id="dateOfBirth" />
+                        <input ref={dateOfBirthRef} type="date" id="dateOfBirth" />
                     </div>
                     <div className="create-employee-form-input">
                         <label htmlFor="startDate">Start date</label>
-                        <input type="date" id="startDate" />
+                        <input ref={startDateRef} type="date" id="startDate" />
                     </div>
                     <div className="create-employee-form-input-group">
                         <span className="create-employee-form-input-group__title">Adress</span>
                         <div className="create-employee-form-input">
                             <label htmlFor="street">Street</label>
-                            <input type="text" id="street" />
+                            <input ref={streetRef} type="text" id="street" />
                         </div>
                         <div className="create-employee-form-input">
                             <label htmlFor="city">City</label>
-                            <input type="text" id="city" />
+                            <input ref={cityRef} type="text" id="city" />
                         </div>
                         <div className="create-employee-form-input">
                             <label htmlFor="state">State</label>
-                            <select id="state">
+                            <select ref={stateRef} id="state">
                                 <option value="1">Alabama</option>
                                 <option value="2">Oklahoma</option>
                                 <option value="3">California</option>
@@ -44,21 +61,21 @@ const NewEmployee: React.FC = () => {
                         </div>
                         <div className="create-employee-form-input">
                             <label htmlFor="zipCode">Zip Code</label>
-                            <input type="text" id="zipCode" />
+                            <input ref={zipCodeRef} type="text" id="zipCode" />
                         </div>
                     </div>
                     <div className="create-employee-form-input">
                         <label htmlFor="department">Department</label>
-                        <select id="department">
-                            <option value="1">IT</option>
-                            <option value="2">HR</option>
-                            <option value="3">Sales</option>
+                        <select ref={departmentRef} id="department">
+                            <option value="1">Sales</option>
                             <option value="4">Marketing</option>
+                            <option value="2">Engineering</option>
+                            <option value="3">Human Resources</option>
+                            <option value="3">Legal</option>
                         </select>
                     </div>
                     <div className="create-employee-form-input-button">
-                    <button className="create-employee-form-button" type="submit">Save</button>
-
+                        <button className="create-employee-form-button" type="submit">Save</button>
                     </div>
                 </form>
             </section>
